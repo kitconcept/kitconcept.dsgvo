6a508a9580485c954eb90470f57a42e641790079
Érico Andrei <ericof@gmail.com>
2022-08-09 15:37:04 -0300

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name="kitconcept.dsgvo",
-    version="2.0.1",
+    version="2.0.2.dev0",
     description="DSGVO / GDPR compliance for Plone",
     long_description=long_description,
     long_description_content_type="text/markdown",
